4732c2365a6169e44fc6924ade3e904dc85a0151
SJCaldwell <shane.caldwell12@ncf.edu>
2018-11-01 22:06:09 -0400

    Update lyric pulling

--- lyric_scraping/lyric_scrape.py
+++ lyric_scraping/lyric_scrape.py
@@ -1,15 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-
+import csv
 
 GENIUS_API_TOKEN = os.environ.get('genius_token')
 
 base_url = "https://api.genius.com"
 headers = {'Authorization': 'Bearer '+ GENIUS_API_TOKEN}
-
-song_title = "Runaway"
-artist_name = "BROCKHAMPTON"
 
 def lyrics_from_song_api_path(song_api_path):
   song_url = base_url + song_api_path
@@ -26,25 +23,48 @@ def lyrics_from_song_api_path(song_api_path):
   lyrics = html.find("div", class_="lyrics").get_text() #updated css where the lyrics are based in HTML
   return lyrics
 
-if __name__ == "__main__":
+def pull_songs_for_artist(artist, genre):
+  """Pulls songs for an artist from genius, and places the result in
+     a directory named after that artist.
+  
+  Arguments:
+    artist {string} -- Artist name, must represent the artist's name on genius
+    genre {string} -- String representing genre (e.g pop/rap)
+  """
   has_results = True
   page_count = 1
-  while has_results:
+  while has_results and page_count < 5:
     has_results = False
     search_url = base_url + "/search"
-    data = {'q': artist_name, 'page': page_count}
+    data = {'q': artist, 'page': page_count}
     page_count +=1
     response = requests.get(search_url, data=data, headers=headers)
     json = response.json()
     song_info = None
     for hit in json["response"]["hits"]:
       has_results = True
-      if hit["result"]["primary_artist"]["name"] == artist_name:
+      if hit["result"]["primary_artist"]["name"] == artist:
         song_info = hit
         title = song_info['result']['title']
         song_api_path = song_info['result']['api_path']
-        filename = title.replace(" ", "").lower()
-        artist_folder = artist_name.replace(" ", "").lower()
-        with open('../data/rap/' + artist_folder + "/" + filename, 'w') as f:
+        filename = title.replace(" ", "").replace("/","").lower()
+        artist_folder = artist.replace(" ", "").replace(",","").lower()
+        target_dir = "../data/" + genre + "/" + artist_folder + "/"
+        if not os.path.exists(target_dir):
+          os.makedirs(target_dir)
+        with open(target_dir + filename, 'w') as f:
           f.write(lyrics_from_song_api_path(song_api_path))
     page_count += 1
+  print("COMPLETED PULL FOR " + str(artist))
+
+def pull_artists_from_file(filename):
+  with open(filename) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)
+    for row in csv_reader:
+      artist = row[0]
+      genre = row[1]
+      pull_songs_for_artist(artist, genre)
+
+if __name__ == "__main__":
+  pull_artists_from_file('artists.csv')
